2373b517048bd8114daf18b6b9ae5dcfd9768dfa
Ricky <93600618@qq.com>
2018-10-26 13:34:15 +0800

    no message

---
+++ pages/personal_info/personal_info.js
@@ -0,0 +1,55 @@
+// pages/personal_info/personal_info.js
+Page({
+  data: {
+    array: ['男', '女'],
+    objectArray: [{
+        id: 0,
+        name: '男'
+      },
+      {
+        id: 1,
+        name: '女'
+      }
+    ],
+    index: 0,
+    date: '2016-09-01'
+  },
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindMultiPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      multiIndex: e.detail.value
+    })
+  },
+  bindMultiPickerColumnChange: function(e) {
+    console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+    var data = {
+      multiArray: this.data.multiArray,
+      multiIndex: this.data.multiIndex
+    };
+    this.setData(data);
+  },
+  bindDateChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  bindRegionChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
+  }
+})
